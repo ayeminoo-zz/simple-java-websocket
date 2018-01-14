fa788f3ee83428feaa01d46a60683bfd7397d477
ayeminoosc@gmail.com <ayeminoosc@gmail.com>
2018-01-14 18:43:04 +0800

    fix the autobahn test cases up to 6.23.7

--- java-websocket/src/main/java/com/amo/utility/BitUtility.java
+++ java-websocket/src/main/java/com/amo/utility/BitUtility.java
@@ -1,7 +1,5 @@
 package com.amo.utility;
 
-import com.sun.deploy.util.StringQuoteUtil;
-import com.sun.xml.internal.ws.util.StringUtils;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.CharacterCodingException;
